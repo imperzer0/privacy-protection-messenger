b42c55dba04b4fa1668b33aa68e1d19f30ac565e
BuildTools <unconfigured@null.spigotmc.org>
2022-03-17 23:47:57 +0200

    defined general interface added colors.hpp

---
+++ color.hpp
@@ -0,0 +1,67 @@
+//
+// Created by imper on 3/17/22.
+//
+
+#ifndef PRIVACY_PROTECTION_MESSENGER_COLOR_HPP
+#define PRIVACY_PROTECTION_MESSENGER_COLOR_HPP
+
+// reset colors and formatting
+#define COLOR_RESET "\033[0m"
+// intense color format
+#define COLOR_INTENSE "\033[1m"
+// faint color format
+#define COLOR_FAINT "\033[2m"
+// italic format
+#define COLOR_ITALIC "\033[3m"
+// underlined format
+#define COLOR_UNDER "\033[4m"
+// blinking text format
+#define COLOR_BLINK "\033[5m"
+// selected text format
+#define COLOR_SELECT "\033[7m"
+// invizible text format
+#define COLOR_INVIZ "\033[8m"
+// strikeout format
+#define COLOR_STRIKE "\033[9m"
+
+// foreground color
+#define COLOR_BLACK "\033[30m"
+// foreground color
+#define COLOR_RED "\033[31m"
+#define COLOR_SEPAR COLOR_RED
+// foreground color
+#define COLOR_GREEN "\033[32m"
+// foreground color
+#define COLOR_YELLOW "\033[33m"
+#define COLOR_HERO COLOR_YELOW
+// foreground color
+#define COLOR_BLUE "\033[34m"
+// foreground color
+#define COLOR_MAGENTA "\033[35m"
+// foreground color
+#define COLOR_CYAN "\033[36m"
+// foreground color
+#define COLOR_WHITE "\033[37m"
+// foreground color
+#define COLOR_DEFAULT "\033[38m"
+
+// background color
+#define COLOR_BLACK_BG "\033[40m"
+// background color
+#define COLOR_RED_BG "\033[41m"
+// background color
+#define COLOR_GREEN_BG "\033[42m"
+// background color
+#define COLOR_YELLOW_BG "\033[43m"
+// background color
+#define COLOR_BLUE_BG "\033[44m"
+// background color
+#define COLOR_MAGENTA_BG "\033[45m"
+// background color
+#define COLOR_CYAN_BG "\033[46m"
+// background color
+#define COLOR_WHITE_BG "\033[47m"
+// background color
+#define COLOR_DEFAULT_BG "\033[48m"
+
+#endif //PRIVACY_PROTECTION_MESSENGER_COLOR_HPP
--- main.cpp
+++ main.cpp
@@ -1,12 +1,70 @@
 #include "network.hpp"
+#include "color.hpp"
+#include <log-console-defs>
 
 #include <iostream>
+#include <getopt.h>
 
+#define HELP COLOR_RESET "Usage: " COLOR_MAGENTA "\"%s\"" COLOR_RESET " -l " COLOR_BLUE "<login>" COLOR_RESET " -p " COLOR_BLUE "<password>" \
+             COLOR_RESET " -m " COLOR_BLUE "<method>" COLOR_RESET " " COLOR_CYAN "[OPTIONS]" COLOR_RESET                                     \
+             "\n" COLOR_RESET COLOR_YELLOW                                                                                                   \
+             "Options:\n"                                                                                                                    \
+             " --method|-m        <method>    messaging action type\n"                                                                       \
+             " --login|-l         <login>     login\n"                                                                                       \
+             " --password|-p      <password>  password\n"                                                                                    \
+             " --version|-v                   print application version\n" COLOR_RESET "\n"
 
-
+static const char* appname = nullptr;
+static constexpr const char* s_options = "m:l:p:v";
+const option l_options[]{
+		{"method",   required_argument, nullptr, 'm'},
+		{"login",    required_argument, nullptr, 'l'},
+		{"password", required_argument, nullptr, 'p'},
+		{"version",  required_argument, nullptr, 'v'},
+		{nullptr}
+};
 
 int main(int argc, char** argv)
 {
+	appname = argv[0];
+	char* login, *password;
+	
+	int opt, longid;
+	while ((opt = ::getopt_long(argc, argv, s_options, l_options, &longid)) >= 0)
+	{
+		switch (opt)
+		{
+			case 'm':
+			{
+				/// TODO: Method parsing
+				break;
+			}
+			
+			case 'l':
+			{
+				login = ::strdup(optarg);
+				break;
+			}
+			
+			case 'p':
+			{
+				password = ::strdup(optarg);
+				break;
+			}
+			
+			case 'v':
+			{
+				::printf("Version: " COLOR_MAGENTA VERSION COLOR_RESET ".\nBinary file path: \"%s\".\n", appname);
+				break;
+			}
+			
+			default: // '?'
+			{
+				::printf(HELP, appname);
+				::exit(-2);
+			}
+		}
+	}
 	
 	return 0;
 }
